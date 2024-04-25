9f099c01ede0a03bc1124608e0fb9cc1cb1cd9d8
Sray1204 <lexpague@gmail.com>
2024-04-26 00:38:19 +0100

    playlist 1

--- quiz.js
+++ quiz.js
@@ -61,40 +61,40 @@ function nextQuestion() {
 
 //localStorage.setItem() then getitem in media player script 
 //will direct to playlist and store the playlist number/ id through the website and then be read and will choose the playlist in the mediaplayer script 
-function calculateMoodPoints(){
+function calculateMoodPoints() {
     let storeId;
 
-    if(happyCount > 4){
-        storeId = 1;
-    }
-    else if(chillCount > 4){
-        storeId = 2;
-    }
-    else if(sadCount > 4){
-        storeId = 3;
-    }
-    else if(angryCount > 4){
-        storeId = 4;
-    }
-    else if(chillCount > 2 && happyCount > 2){
+    // Check for mood combinations
+    if (chillCount > 2 && happyCount > 2) {
         storeId = 5;
-    }
-    else if(sadCount > 2 && angryCount > 2){
+    } else if (sadCount > 2 && angryCount > 2) {
         storeId = 6;
-    }
-    else if (sadCount > 2 && chillCount > 1 && angryCount > 2){
+    } else if (sadCount > 2 && chillCount > 1 && angryCount > 2) {
         storeId = 7;
+    } else {
+        // Check individual mood counts
+        if (happyCount > 4) {
+            storeId = 2;
+        } else if (sadCount > 4) {
+            storeId = 3;
+        } else if (angryCount > 4) {
+            storeId = 4;
+        } else if (chillCount > 4) {
+            storeId = 1;
+        } else {
+            // Default storeId if none of the conditions are met
+            storeId = 2;
+        }
     }
-    else{
-        storeId = 2;
-    }
- 
-    //Store the playlist ID in local storage
-    localStorage.setItem('playlistId', storeId);
-    //alert("Playlist ID: " + storeId);
 
+    // Store the playlist ID in local storage
+    localStorage.setItem('playlistId', storeId);
+    alert("Playlist ID: " + storeId);
+
+    // Redirect to the corresponding playlist page
     window.location.replace("playlist-page/happy.html");
 }
+
 
 
 
